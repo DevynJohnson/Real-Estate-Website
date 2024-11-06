87bc79d1ad85b70c33e019fde8a4215889948ef8
Devyn Johnson <dljohnson1313@gmail.com>
2024-11-06 16:03:49 -0600

    adjust nav bar functionality, add image library for hamburger menu, add js so nav bar collapses on mobile to menu icon

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav-menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-menu";
+    }
+}
